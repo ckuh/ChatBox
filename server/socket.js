b4ac4e680db07b5ff1b67bea50861dfc4521621b
Chris Kuh <kuh_chris@yahoo.com>
2016-03-18 19:23:35 -0700

    [clean] remove usercount

--- server/socket.js
+++ server/socket.js
@@ -1,16 +1,16 @@
 var getUserDataHelper = require('./helperFunc/getUserDataHelper');
 
 module.exports = function(io) {
-  // var userCount = 0;
+
   io.on('connection', function(socket) {
-    // userCount++;
+
     socket.on('disconnect', function() {
-      // userCount--;
+
       io.emit('user left')
     });
 
     socket.on('userLogin', function(user) {
-      // user.userCount = userCount
+
       io.to(socket.id).emit('userLogin', user);
     })
 
